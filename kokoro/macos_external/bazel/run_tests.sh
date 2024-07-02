c1eadcafc58f6bf40df2a83bd70c76f4cebb2baf
Moreno Ambrosin <ambrosin@google.com>
2024-07-02 05:26:26 -0700

    Install a more recent version of the Android cmdline-tools in macOS tests
    PiperOrigin-RevId: 648683213
    Change-Id: I0e5ce8d65c3f285582bb07856856fa79e31165e8

--- kokoro/macos_external/bazel/run_tests.sh
+++ kokoro/macos_external/bazel/run_tests.sh
@@ -16,20 +16,17 @@
 
 set -euo pipefail
 
-export XCODE_VERSION="14.1"
-export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
-export ANDROID_HOME="/usr/local/share/android-sdk"
-export COURSIER_OPTS="-Djava.net.preferIPv6Addresses=true"
-
 if [[ -n "${KOKORO_ROOT:-}" ]] ; then
   readonly TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
   cd "${TINK_BASE_DIR}/tink_java_gcpkms"
   export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-8-latest/Contents/Home
+  export XCODE_VERSION="14.1"
+  export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
 fi
 
 ./kokoro/testutils/copy_credentials.sh "testdata" "gcp"
 ./kokoro/testutils/copy_credentials.sh "examples/testdata" "gcp"
-./kokoro/testutils/update_android_sdk.sh
+source ./kokoro/testutils/update_android_sdk.sh
 
 # Run manual tests which rely on key material injected into the Kokoro
 # environement.
